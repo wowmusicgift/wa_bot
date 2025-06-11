3f7a98fed92732af21e3a7c5dfb83e7ac0eeb331
wowmusicgift <wow.music.gift@gmail.com>
2025-06-12 02:10:45 +0500

    0.0.75

--- main.py
+++ main.py
@@ -119,8 +119,8 @@ def send_message(to, text, platform="whatsapp"):
             # Универсальная замена номера с +77 на 87
             if to.startswith("+77"):
                 to = "787" + to[3:]
-                print("CORRECT WA_ID: ", to)
-
+                
+            print("CORRECT WA_ID: ", to)
             data = {
                 "messaging_product": "whatsapp",
                 "to": to,
